068e16b5434a87091aa6252fed2bd3026a7961ce
Florian Gabriel <flow@p0cki.net>
2023-03-06 20:15:09 +0100

    Kapitel 4/42: Product Details, Categories & Seeding More Products.

---
+++ UdemyBlazorECommerceWebsite/Shared/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyBlazorECommerceWebsite.Shared
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+    }
+}
